6033870fdf8531f3f93df5ef136856f5ccc59679
tonywarwick69 <duc.khang165@gmail.com>
2021-11-27 16:03:17 +0700

    tempest-connect

---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,101 @@
+import { useEffect } from 'react';
+import { useChat } from 'context';
+import { getChats, ChatEngine } from 'react-chat-engine';
+import { LeftRail, ChatToolbar, ChatInput, MessageList } from 'components';
+import { Icon } from 'semantic-ui-react';
+
+export const Chat = () => {
+  const {
+    myChats,
+    setMyChats,
+    chatConfig,
+    selectedChat,
+    selectChatClick,
+    setSelectedChat,
+    closeChat,
+  } = useChat();
+
+  useEffect(() => {
+    console.log('My Chats: ', myChats);
+  }, [myChats]);
+
+  useEffect(() => {
+    console.log('Selected Chat: ', selectedChat);
+  }, [selectedChat]);
+  //closechat
+
+  useEffect(()=>{
+    console.log('Close chat', closeChat);
+    //closeChat={()=> ChatEngine(false)}
+  }, [closeChat]);
+ 
+  return (
+    <>
+      {!!chatConfig && (
+        <ChatEngine
+          hideUI={true}
+          userName={chatConfig.userName}
+          projectID={chatConfig.projectID}
+          userSecret={chatConfig.userSecret}
+          onConnect={() => {
+            getChats(chatConfig, setMyChats);
+          }}
+          onNewChat={chat => {
+            if (chat.admin.username === chatConfig.userName) {
+              selectChatClick(chat);
+            }
+            setMyChats([...myChats, chat].sort((a, b) => a.id - b.id));
+          }}
+          onDeleteChat={chat => {
+            if (selectedChat?.id === chat.id) {
+              setSelectedChat(null);
+            }
+            setMyChats(
+              myChats.filter(c => c.id !== chat.id).sort((a, b) => a.id - b.id),
+            );
+          }}
+          onNewMessage={(chatId, message) => {
+            if (selectedChat && chatId === selectedChat.id) {
+              setSelectedChat({
+                ...selectedChat,
+                messages: [...selectedChat.messages, message],
+              });
+            }
+            const chatThatMessageBelongsTo = myChats.find(c => c.id === chatId);
+            const filteredChats = myChats.filter(c => c.id !== chatId);
+            const updatedChat = {
+              ...chatThatMessageBelongsTo,
+              last_message: message,
+            };
+            setMyChats(
+              [updatedChat, ...filteredChats].sort((a, b) => a.id - b.id),
+            );
+          }}
+          
+        />
+      )}
+    
+      <div className="chat-container">
+        <LeftRail />
+        <div className="current-chat">
+          {selectedChat ? (
+            <div className="chat">
+              <ChatToolbar />
+              <MessageList />
+              <ChatInput />
+            </div>
+          ) : (
+            <div className="no-chat-selected">
+              <img
+                src="/img/pointLeft.png"
+                className="point-left"
+                alt="point-left"
+              />
+              Select A Chat
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
